399ecbad473ad004f8f9dc09a82eadb7d7e057da
abdel <aitsiahmad.abdel@gmail.com>
2014-12-08 17:24:38 +0000

    scanneur de code

---
+++ Scanner/src/com/asa/mcommerce/MainActivity.java
@@ -0,0 +1,74 @@
+package com.asa.mcommerce;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends Activity implements OnClickListener{
+	private Button scanBtn;
+	private TextView formatTxt, contentTxt;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		scanBtn = (Button)findViewById(R.id.btn_scan);
+		formatTxt = (TextView)findViewById(R.id.scan_format);
+		contentTxt = (TextView)findViewById(R.id.scan_content);
+		
+		scanBtn.setOnClickListener((OnClickListener) this);
+		
+	}
+
+	public void onClick(View v){
+		if(v.getId()==R.id.btn_scan){
+			//scan
+			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+			scanIntegrator.initiateScan();
+			}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if (scanningResult != null) {
+			String scanContent = scanningResult.getContents();
+			String scanFormat = scanningResult.getFormatName();
+			formatTxt.setText("FORMAT: " + scanFormat);
+			contentTxt.setText("CONTENTgdgg: " + scanContent);
+			}
+		else{
+		    Toast toast = Toast.makeText(getApplicationContext(), 
+		        "No scan data received!", Toast.LENGTH_SHORT);
+		    toast.show();
+		}
+	}
+}
